4768a296ab61a310230ef70b2a6103674f5ee81b
michealstubbs-ship-it <micheal.stubbs@gmail.com>
2026-06-26 14:04:59 +0400

    Global search bar (contacts/companies/signals/leads); Companies A-Z + sort; Signals unactioned-first

--- force-push.bat
+++ force-push.bat
@@ -29,7 +29,7 @@ cd /d "%WORK%"
 git config user.email "micheal.stubbs@gmail.com"
 git config user.name "michealstubbs-ship-it"
 git add -A
-git commit -m "UX: All Contacts A-Z sort + sort dropdown; Jobs active-first ordering"
+git commit -m "Global search bar (contacts/companies/signals/leads); Companies A-Z + sort; Signals unactioned-first"
 if %errorlevel% neq 0 (echo Nothing to commit. & goto done)
 
 echo [4/4] Pushing...
